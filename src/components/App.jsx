92b06d4adc88ef303c26a27ddbf45acadfda49ee
Preksler <preksler@gmail.com>
2022-07-08 15:15:42 +0300

    add localstorage to phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,13 +8,26 @@ import Container from "./Container/Container";
 
 export class App extends Component {
   state = {
-    contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
+    contacts: [],
     filter: '',
+  }
+
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts) {
+      try {
+        this.setState({ contacts: JSON.parse(savedContacts) });
+      }
+      catch (e) {
+        console.log(e);
+      }
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
   }
 
   formSubmit = ({ name, number }) => {
@@ -74,22 +87,3 @@ export class App extends Component {
     )
   }
 }
-
-
-
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101'
-//       }}
-//     >
-//       React homework template
-//     </div>
-//   );
-// };
